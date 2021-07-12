7459d925077c5ae9735a05bf528cfcd95b89ea20
jingx <jingx@ickey.cn>
2021-07-12 22:34:58 +0800

    update

---
+++ selfTest/context/demo01/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "time"
+)
+
+func main(){
+
+    ctx ,cancel := context.WithCancel(context.Background())
+
+    go func() {
+
+        defer func() {
+            fmt.Println("goroutine exit")
+        }()
+
+        for {
+            select {
+
+            case <-ctx.Done():
+                fmt.Println(ctx.Err())
+                return
+            default:
+                time.Sleep(time.Second)
+            }
+        }
+
+    }()
+
+    time.Sleep(time.Second)
+    cancel()
+    time.Sleep(2 * time.Second)
+
+
+
+}
--- selfTest/context/demo04/main.go
+++ selfTest/context/demo04/main.go
@@ -1,9 +1,19 @@
 package main
 
-
+import (
+    "context"
+    "fmt"
+)
 
 func main(){
 
+        ctx := context.TODO()
 
-	
+        ctx = context.WithValue(ctx,"key1","val1")
+        ctx = context.WithValue(ctx,"key2","val2")
+        ctx = context.WithValue(ctx,"key3","val3")
+        ctx = context.WithValue(ctx,"key4","val4")
+
+        fmt.Println(ctx.Value("key1"))
+
 }
